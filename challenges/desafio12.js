3a1a84261bc389a86f57068af122e851469eb790
GustavoPD <gudalmoro@gmail.com>
2022-07-08 02:31:36 -0300

    req 10-14

---
+++ challenges/desafio12.js
@@ -0,0 +1,7 @@
+db.produtos
+  .updateMany(
+    { $nor: [{ nome: "McChicken" }] },
+    { $addToSet: { ingredientes: "ketchup" } },
+  );
+db.produtos
+  .find({}, { nome: true, ingredientes: true, _id: false });
